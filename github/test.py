419688b7af7707659972ee6a81394957c89b4d7b
leemengtaiwan <b98705001@gmail.com>
2018-01-22 21:48:00 +0900

    Add helper to query github GraphQL API

---
+++ github/test.py
@@ -0,0 +1,26 @@
+import time
+import sys
+import unittest
+import util
+
+
+class Test(unittest.TestCase):
+    """ Test Github GraphQL API"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_access(self):
+        """Test simple access to Github GraphQL API"""
+        pass
+
+        query = "{\"query\":\"query {\\n  viewer {\\n    login\\n  }\\n}\"}"
+        print(util.query_graphql(query))
+
+
+
+if __name__ == "__main__":
+    unittest.main(argv=[sys.argv[0]])
---
+++ github/util.py
@@ -0,0 +1,17 @@
+import fire
+import requests
+from secret import BEARER_TOKEN
+
+
+def query_graphql(payload, token=BEARER_TOKEN, url='https://api.github.com/graphql'):
+    headers = {
+        'content-type': "application/json",
+        'authorization': "Bearer {}".format(BEARER_TOKEN)
+    }
+
+    r = requests.request("POST", url, data=payload, headers=headers)
+    return r.text
+
+
+if __name__ == '__main__':
+    fire()
